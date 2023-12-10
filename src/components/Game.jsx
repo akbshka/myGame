421fa5eebca800b01555fff04017b82d29e2220b
akbshka <152325149+akbshka@users.noreply.github.com>
2023-12-10 22:31:39 +0300

    krestiki

---
+++ src/components/Game.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Board from './Board'
+import {calculateWinner} from '../helper'
+
+import './Game.css'
+
+
+const Game = () => {
+    const [board, setBoard] = useState(Array(9).fill(null))
+    const [xIsNext, setXIsNext] = useState(true)
+    const winner = calculateWinner(board)
+
+    const handleClick = (index) => {
+        const boardCopy = [...board]
+
+        if (winner || boardCopy[index]) return
+
+        boardCopy[index] = xIsNext ? 'X' : '0'
+
+        setBoard(boardCopy)
+        setXIsNext(!xIsNext)
+
+    }
+    
+    const startNewGame = () => {
+        return (
+            <button clasName="start__btn" onClick={() => setBoard(Array(9).fill(null))}> Очистить поле</button>
+        )
+    }
+
+    return (
+        <div className="wrapper">
+            { startNewGame() }
+            <Board squares={board} click={handleClick}/>
+            <p>
+                { winner ? 'Победитель' + winner : 'Сейчас ходит ' + ( xIsNext ? 'X' : '0') }
+            </p>
+        </div>
+    );
+}
+
+export default Game;
